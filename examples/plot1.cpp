94846ef66dbca5c5e0b328c47402e303ffcfcd8d
HumaSOFT <humasoft@ing.uc3m.es>
2019-03-20 20:05:25 +0100

    modificado:    CMakeLists.txt 	modificado:    examples/plot1.cpp 	modificado:    lib/i-plot

--- examples/plot1.cpp
+++ examples/plot1.cpp
@@ -1,1 +1,29 @@
 
+#include <complex>
+
+#include "fcontrol.h"
+#include <math.h>
+
+#include "IPlot.h"
+
+using namespace std;
+
+int main()
+{
+
+
+    double dts=0.01;
+    IPlot p(dts);
+
+    for (double t=0; t<2; t+=0.01)
+
+    {
+        p.pushBack(t*0.1);
+
+    }
+
+
+    p.Plot();
+
+    return 0;
+}
